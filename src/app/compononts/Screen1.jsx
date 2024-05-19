4dc20a8d72e710271768dedac6481ba39c26279c
muhammadharits23 <muhammadharitsuliman@gmail.com>
2024-05-19 17:43:47 +0700

    screen1

---
+++ src/app/compononts/Screen1.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Screen1() {
+  return (
+    <div className="bg-[#68C7FF]">
+      <div className="flex items-center min-h-[700px]">
+        <div className="w-[50%]"></div>
+        <div className="flex flex-col items-center w-[50%]">
+          <h5 className="text-red-500 bg-white py-2 px-5">DEMPSEY + DELTA</h5>
+          <h3 className="text-sky-900 text-[50px] font-semibold text-center">
+            Sustainability Initiatives
+          </h3>
+          <h6 className="text-black mt-10">Sustainable Progress Goals</h6>
+          <h6 className="text-black">Report December 2025</h6>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Screen1;
