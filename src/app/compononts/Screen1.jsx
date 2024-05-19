e910bedee427ed52a993277277a0b4102cfa45cd
muhammadharits23 <muhammadharitsuliman@gmail.com>
2024-05-19 17:53:48 +0700

    Revert "cek commit screen1"
    This reverts commit 9a3ab0b3cb3bc9a41296ed1cbeec5b631e65a5ba.

--- src/app/compononts/Screen1.jsx
+++ src/app/compononts/Screen1.jsx
@@ -7,7 +7,9 @@ function Screen1() {
         <div className="w-[50%]"></div>
         <div className="flex flex-col items-center w-[50%]">
           <h5 className="text-red-500 bg-white py-2 px-5">DEMPSEY + DELTA</h5>
-
+          <h3 className="text-sky-900 text-[50px] font-semibold text-center">
+            Sustainability Initiatives
+          </h3>
           <h6 className="text-black mt-10">Sustainable Progress Goals</h6>
           <h6 className="text-black">Report December 2025</h6>
         </div>
