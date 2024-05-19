5e3510a3154fe317eaba795a88ac401a9251f3b0
muhammadharits23 <muhammadharitsuliman@gmail.com>
2024-05-19 17:44:21 +0700

    screen4

---
+++ src/app/compononts/Screen4.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Screen4() {
+  return (
+    <div className="bg-[#68C7FF]">
+      <div className="flex items-center min-h-[700px]">
+        <div className="flex flex-col items-center w-[50%] text-sky-900">
+          <h3 className="text-[20px] font-semibold text-center">
+            Massage From Our Leader
+          </h3>
+          <h5>
+            Make your employees feel welcome and valued with a carefully crafted
+            opening message. Aside from serving as a greeting, this space is
+            also great for showing what your company culture is like by example.
+            Share your thoughts on the company's beginnings and growth. Mention
+            your vision for the company. Remember to express your appreciation
+            and willingness to listen to everyone's ideas. And finally, remind
+            each person how important they are to the bigger group.
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Screen4;
