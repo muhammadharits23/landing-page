06dc96e55fc5c4b76685be3f807682a1f5a04e6c
muhammadharits23 <muhammadharitsuliman@gmail.com>
2024-05-19 17:42:57 +0700

    init projek

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "totalcrane.co.id",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
